5410f31558e4a88fe5d9cb8b6b4d61dec3e3799b
Dhanushan <dhanushan75@gmail.com>
2021-04-12 15:10:40 +0530

    added plugin support to the webscraping

--- web-scrapping-selenium.py
+++ web-scrapping-selenium.py
@@ -11,7 +11,11 @@ import time
 
 opts = Options()
 opts.BinaryLocation = "/usr/bin/chromium-browser"
-#Remove this comment if you dont want see the UI pop up
+
+#Below line to add extension privacy pass which has been packed as crx file
+opts.add_extension('./2.0.7_0.crx')
+
+#Remove this comment if you dont want see the UI pop up also removing helps in masking from anti bot programs
 #opts.headless =True
 chromedriverPath = '/home/admin1/Documents/webscraping/Python-Webscrapping/chromedriver'
 driver = webdriver.Chrome(executable_path=chromedriverPath,options=opts)
@@ -23,7 +27,10 @@ else:
 
 URL1 = 'https://dramacool.vc/365-repeat-the-year-episode-3.html'
 URL = 'https://dramacool.vc/drama-detail/365-one-year-against-destiny'
-testurl = 'https://www.google.com/'
 driver.get(URL1)
 
-print(driver)
+button = driver.find_elements_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[4]/ul/li[4]')[0]
+button.click()
+
+#list_of_download_elements = driver.find_element_by_name("download")
+#print(list_of_download_elements)
