0258c51c5f04c201ee5c00a3e6c5a0039bca4198
dhanushanm <dhanushanm@smartenspaces.com>
2021-06-12 20:39:10 +0530

    added new chrome driver for version 91

--- web-scrapping-selenium.py
+++ web-scrapping-selenium.py
@@ -10,14 +10,14 @@ import time
 #driver = webdriver.Chrome(ChromeDriverManager().install())
 
 opts = Options()
-opts.BinaryLocation = "/usr/bin/chromium-browser"
+#opts.BinaryLocation = "/usr/bin/chromium-browser"
 
 #Below line to add extension privacy pass which has been packed as crx file
 opts.add_extension('./2.0.7_0.crx')
 
 #Remove this comment if you dont want see the UI pop up also removing helps in masking from anti bot programs
 #opts.headless =True
-chromedriverPath = '/home/admin1/Documents/webscraping/Python-Webscrapping/chromedriver'
+chromedriverPath = './chromedriver'
 driver = webdriver.Chrome(executable_path=chromedriverPath,options=opts)
 
 if(driver == None):
@@ -29,7 +29,7 @@ URL1 = 'https://dramacool.vc/365-repeat-the-year-episode-3.html'
 URL = 'https://dramacool.vc/drama-detail/365-one-year-against-destiny'
 driver.get(URL1)
 
-button = driver.find_elements_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[4]/ul/li[4]')[0]
+button = driver.find_elements_by_xpath('/html/body/div[1]/div/div[1]/div[2]/div[4]/ul/li[4]/a')[0]
 button.click()
 
 #list_of_download_elements = driver.find_element_by_name("download")
